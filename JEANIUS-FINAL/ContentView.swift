a3f9cb3eece1d3f81f15a755b0c6accc502f456d
Samantha Wong <samanthaw9876@gmail.com>
2024-08-01 13:45:18 -0700

    Initial Commit

---
+++ JEANIUS-FINAL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JEANIUS-FINAL
+//
+//  Created by Scholar on 8/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JEANIUS-FINAL/JEANIUS_FINALApp.swift
@@ -0,0 +1,17 @@
+//
+//  JEANIUS_FINALApp.swift
+//  JEANIUS-FINAL
+//
+//  Created by Scholar on 8/1/24.
+//
+
+import SwiftUI
+
+@main
+struct JEANIUS_FINALApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
